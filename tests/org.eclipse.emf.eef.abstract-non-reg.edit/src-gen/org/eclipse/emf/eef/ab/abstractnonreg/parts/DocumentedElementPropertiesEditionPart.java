5c61483663c922093976318d6edbd9869626f2f3
sbouchet <sbouchet>
2010-08-27 15:23:29 +0000

    regen with latest templates

---
+++ tests/org.eclipse.emf.eef.abstract-non-reg.edit/src-gen/org/eclipse/emf/eef/ab/abstractnonreg/parts/DocumentedElementPropertiesEditionPart.java
@@ -0,0 +1,45 @@
+/**
+ * Generated with Acceleo
+ */
+package org.eclipse.emf.eef.ab.abstractnonreg.parts;
+
+// Start of user code for imports
+
+// End of user code
+
+/**
+ * 
+ * 
+ */
+public interface DocumentedElementPropertiesEditionPart {
+
+	/**
+	 * @return the documentation
+	 * 
+	 */
+	public String getDocumentation();
+
+	/**
+	 * Defines a new documentation
+	 * @param newValue the new documentation to set
+	 * 
+	 */
+	public void setDocumentation(String newValue);
+
+
+
+
+
+	/**
+	 * Returns the internationalized title text.
+	 * 
+	 * @return the internationalized title text.
+	 * 
+	 */
+	public String getTitle();
+
+	// Start of user code for additional methods
+	
+	// End of user code
+
+}
