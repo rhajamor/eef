53b5d00637b5f51853622b3db099513c4b6a6817
sbouchet <sbouchet>
2010-08-30 08:51:54 +0000

    cleanup

---
+++ tests/org.eclipse.emf.examples.eef/src-gen/org/eclipse/emf/samples/conference/impl/ConferenceFactoryImpl.java
@@ -0,0 +1,223 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: ConferenceFactoryImpl.java,v 1.1.4.1 2010/08/30 08:51:54 sbouchet Exp $
+ */
+package org.eclipse.emf.samples.conference.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.samples.conference.Conference;
+import org.eclipse.emf.samples.conference.ConferenceFactory;
+import org.eclipse.emf.samples.conference.ConferencePackage;
+import org.eclipse.emf.samples.conference.GENDER;
+import org.eclipse.emf.samples.conference.Person;
+import org.eclipse.emf.samples.conference.Site;
+import org.eclipse.emf.samples.conference.TALK_TYPE;
+import org.eclipse.emf.samples.conference.Talk;
+import org.eclipse.emf.samples.conference.Topic;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ConferenceFactoryImpl extends EFactoryImpl implements ConferenceFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static ConferenceFactory init() {
+		try {
+			ConferenceFactory theConferenceFactory = (ConferenceFactory)EPackage.Registry.INSTANCE.getEFactory("http://www.eclipse.org/emf/samples/eef/1.0.0"); 
+			if (theConferenceFactory != null) {
+				return theConferenceFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new ConferenceFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ConferenceFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case ConferencePackage.CONFERENCE: return createConference();
+			case ConferencePackage.PERSON: return createPerson();
+			case ConferencePackage.TALK: return createTalk();
+			case ConferencePackage.TOPIC: return createTopic();
+			case ConferencePackage.SITE: return createSite();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object createFromString(EDataType eDataType, String initialValue) {
+		switch (eDataType.getClassifierID()) {
+			case ConferencePackage.TALK_TYPE:
+				return createTALK_TYPEFromString(eDataType, initialValue);
+			case ConferencePackage.GENDER:
+				return createGENDERFromString(eDataType, initialValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String convertToString(EDataType eDataType, Object instanceValue) {
+		switch (eDataType.getClassifierID()) {
+			case ConferencePackage.TALK_TYPE:
+				return convertTALK_TYPEToString(eDataType, instanceValue);
+			case ConferencePackage.GENDER:
+				return convertGENDERToString(eDataType, instanceValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Conference createConference() {
+		ConferenceImpl conference = new ConferenceImpl();
+		return conference;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Person createPerson() {
+		PersonImpl person = new PersonImpl();
+		return person;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Talk createTalk() {
+		TalkImpl talk = new TalkImpl();
+		return talk;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Topic createTopic() {
+		TopicImpl topic = new TopicImpl();
+		return topic;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Site createSite() {
+		SiteImpl site = new SiteImpl();
+		return site;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TALK_TYPE createTALK_TYPEFromString(EDataType eDataType, String initialValue) {
+		TALK_TYPE result = TALK_TYPE.get(initialValue);
+		if (result == null) throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'");
+		return result;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertTALK_TYPEToString(EDataType eDataType, Object instanceValue) {
+		return instanceValue == null ? null : instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public GENDER createGENDERFromString(EDataType eDataType, String initialValue) {
+		GENDER result = GENDER.get(initialValue);
+		if (result == null) throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'");
+		return result;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertGENDERToString(EDataType eDataType, Object instanceValue) {
+		return instanceValue == null ? null : instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ConferencePackage getConferencePackage() {
+		return (ConferencePackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static ConferencePackage getPackage() {
+		return ConferencePackage.eINSTANCE;
+	}
+
+} //ConferenceFactoryImpl
